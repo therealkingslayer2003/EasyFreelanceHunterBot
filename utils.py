2e0ae5a9dc530ecde0ebec09d921d1ae1c558203
Amir <natural6t@gmail.com>
2022-08-16 17:19:48 +0300

    Add convert function. Uses parsing for each converting, so needs to be improved to parse actual course once per day and then reuse it.

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 # форматирование текста для избежания проблем вывода
 def edited(text):
     return text.replace("_", "\_") \
@@ -25,3 +28,21 @@ def is_valid(text):
     if all(x.isspace() or x.isalnum() for x in text):
         return True
     return False
+
+
+# конвертация валют.
+def convert(amount: float, input_currency: str, output_currency: str):
+    # динамическое заполнение URL
+    url = f"https://www.xe.com/currencyconverter/convert/?Amount={amount}&From={input_currency}&To={output_currency}"
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    # получение конвертированной валюты по актуальному курсу
+    converted_amount = soup.find("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod").text.split(".")
+
+    # корректное отображение
+    converted_amount = converted_amount[0] + "." + converted_amount[1][:2]
+
+    return converted_amount
+
