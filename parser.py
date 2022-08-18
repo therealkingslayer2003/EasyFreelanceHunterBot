d47f14b99450e6ab4fbeed35532fee07c5613eb9
Ruslan <krutiporohruslan@gmail.com>
2022-08-18 21:49:29 +0400

    new test file of parser

--- parser.py
+++ parser.py
@@ -1,6 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
-import html
-import re
 
+
+def get_orders():
+    url = 'https://kwork.ru/projects?fc=41&attr=211'
+    query = requests.get(url)
+
+    soup = BeautifulSoup(query.text, 'lxml')
+
+    orders = soup.findAll('div', class_='card__content pb5')
+
+    #title_and_prices = []
+
+    for order in orders:
+        order_title = order.find('div', class_='mb15').find('div', class_='d-flex relative'). \
+            find('div', class_='wants-card__left').find('div',
+                                                        class_='wants-card__header-title first-letter breakwords pr250'). \
+            find('a').text.strip()
+        order_price = order.find('div', class_='mb15').find('div', class_='d-flex relative').find('div', class_='wants-card__header-right-block').find('div', class_='wants-card__header-controls projects-list__icons'). \
+            find('div', class_='wants-card__header-price wants-card__price m-hidden').text.strip()
+        print(f'{order_title} , {order_price}\n')
+
+
+get_orders()
